3f557a5455c43947d2b1e742fd6a198becd61efa
iRyann <ryanbouchou@orange.fr>
2025-05-29 19:45:31 +0200

    Treesitter auto install, none-ls for linter & formatting via lsp communication.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ":TSUpdate",
     lazy = false,
     opts = {
-      ensure_installed = { "lua", "javascript", "python", "c", "go", "sql", "java" },
+      auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
