ca45c5652707a1b22cffae26b681820bf05eda53
iRyann <ryanbouchou@orange.fr>
2025-06-06 22:07:19 +0200

    lazydev ? and alpha

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -43,6 +43,7 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
+          { name = "lazydev" },
 				}, {
 					{ name = "buffer" },
 				}),
--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,50 +1,58 @@
-return {
+return { 
+  {
   "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		require("nio")
+		local dap, dapui = require("dap"), require("dapui")
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		vim.keymap.set("n", "<Leader>dc", function()
+			dap.continue()
+		end)
+		vim.keymap.set("n", "<Leader>dso", function()
+			dap.step_over()
+		end)
+		vim.keymap.set("n", "<Leader>dsi", function()
+			dap.step_into()
+		end)
+		vim.keymap.set("n", "<Leader>dsq", function()
+			require("dap").step_out()
+		end)
+		vim.keymap.set("n", "<Leader>b", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>B", function()
+			dap.set_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>lp", function()
+			dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+		end)
+		vim.keymap.set("n", "<Leader>dr", function()
+			dap.repl.open()
+		end)
+		vim.keymap.set("n", "<Leader>dl", function()
+			dap.run_last()
+		end)
+	end,
   },
-  config = function()
-    require("nio")
-    local dap, dapui = require("dap"), require("dapui")
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function ()
+      require("dap-python").setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
     end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-    vim.keymap.set("n", "<F5>", function()
-      dap.continue()
-    end)
-    vim.keymap.set("n", "<F10>", function()
-      dap.step_over()
-    end)
-    vim.keymap.set("n", "<F11>", function()
-      dap.step_into()
-    end)
-    vim.keymap.set("n", "<F12>", function()
-      require("dap").step_out()
-    end)
-    vim.keymap.set("n", "<Leader>b", function()
-      dap.toggle_breakpoint()
-    end)
-    vim.keymap.set("n", "<Leader>B", function()
-      dap.set_breakpoint()
-    end)
-    vim.keymap.set("n", "<Leader>lp", function()
-      dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-    end)
-    vim.keymap.set("n", "<Leader>dr", function()
-      dap.repl.open()
-    end)
-    vim.keymap.set("n", "<Leader>dl", function()
-      dap.run_last()
-    end)
-  end,
+  }
 }
