afb24dbb7ee12f97877fcde88e68ef7fd1906edd
Ryan Bouchou <ryanbouchou@orange.fr>
2025-10-03 21:08:48 +0200

    Neotree : autoclose and border style

---
+++ nvim/.config/nvim/lua/plugins/md.lua
@@ -0,0 +1,10 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
+	config = function(_, opts)
+		require("render-markdown").setup(opts)
+	end,
+}
